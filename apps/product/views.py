0ab5616dfeb4db687c67dead2640a6faf8b6abe2
Your Name <you@example.com>
2024-02-19 22:35:23 +0600

    CartUpdate

--- apps/product/views.py
+++ apps/product/views.py
@@ -91,3 +91,15 @@ class CartAPIView(APIView):
         snippet = Cart.objects.get(user_id=id)
         serializer = CartSerializer(snippet)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class CartUpdateAPIView(APIView):
+    permission_classes = [permissions.AllowAny]
+
+    def put(self, request, id):
+        snippet = Cart.objects.get(user_id=id)
+        serializer = CartUpdateSerializer(snippet, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
