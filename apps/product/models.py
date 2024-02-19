57b7f90d8cea2de473d6b9c0883d5912f1b08265
Your Name <you@example.com>
2024-02-19 20:23:04 +0600

    Category

--- apps/product/models.py
+++ apps/product/models.py
@@ -1,3 +1,11 @@
-from django.db import models
+from datetime import datetime
 
-# Create your models here.
+from django.db import models
+from apps.user.models import User
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, null=False)
+
+    def __str__(self):
+        return f'{self.name}'
