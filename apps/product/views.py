7fe41d886daadb8e269430ec59adb0ef713020d3
Your Name <you@example.com>
2024-02-19 20:47:51 +0600

    CategoryCreate

--- apps/product/views.py
+++ apps/product/views.py
@@ -34,3 +34,18 @@ class CategoryListAPIView(generics.ListAPIView):
     permission_classes = [permissions.AllowAny]
     serializer_class = CategorySerializer
     queryset = Category.objects.all()
+
+
+class CategoryCreateAPIView(APIView):
+    permission_classes = [permissions.AllowAny]
+
+    def post(self, request):
+        data = request.data.get('data', [])
+        for category_data in data:
+            serializer = CategorySerializer(data=category_data)
+            if serializer.is_valid():
+                name = category_data['name']
+                Category.objects.get_or_create(name=name)
+        return Response({'response': 'created'}, status=status.HTTP_201_CREATED)
+
+
