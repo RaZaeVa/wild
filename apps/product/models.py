765972333a6092d18c91f9eded70ac6836a0893b
Your Name <you@example.com>
2024-02-19 20:27:45 +0600

    Product

--- apps/product/models.py
+++ apps/product/models.py
@@ -9,3 +9,16 @@ class Category(models.Model):
 
     def __str__(self):
         return f'{self.name}'
+
+
+class Product(models.Model):
+    seller = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=255, null=False)
+    description = models.TextField()
+    price = models.IntegerField()
+    quantity = models.IntegerField()
+    timestamp = models.DateTimeField(default=datetime.now)
+
+    def __str__(self):
+        return f'{self.name}, {self.id}'
